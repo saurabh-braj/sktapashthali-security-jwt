630600aa59172fb6a7093845c41be05ae991e8c6
saurabh <saurabh.braj@gmail.com>
2021-12-18 19:15:20 +0530

    Add Initial Project for JWT Security

---
+++ src/main/java/com/sk/security/SktapashthaliSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.sk.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SktapashthaliSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SktapashthaliSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/security/SktapashthaliSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SktapashthaliSecurityJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
